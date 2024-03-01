0a3f3641c6be357f3f6c0cbf1037a76bd814ab66
ThiagoLuiis <thiago.luis4656@gmail.com>
2024-03-01 15:24:46 -0300

    Alteraçõres Questão B

--- QuestãoB/ModeloBasico.java
+++ QuestãoB/ModeloBasico.java
@@ -6,25 +6,25 @@ public class ModeloBasico implements Celular {
     @Override
     public void ligar() {
         // TODO Auto-generated method stub
-        System.out.println("Ligando Telefone ...");
+        System.out.println("Ligando Telefone Basico ...");
     }
 
     @Override
     public void desligar() {
         // TODO Auto-generated method stub
-        System.out.println("Desligando Telefone ...'");
+        System.out.println("Desligando Telefone Basico ...'");
     }
 
     @Override
     public void fazerLigacao(String numero) {
         // TODO Auto-generated method stub
-        System.out.println("Fazendo Ligação ...");
+        System.out.println("Fazendo Ligação para:" + numero + " usando celular Basico");
     }
 
     @Override
     public void enviarMensagem(String numero, String mensagem) {
         // TODO Auto-generated method stub
-        System.out.println("Enviando Mensgem ...");
+        System.out.println("Enviando Mensgem para " + numero +": "+ mensagem);
 
     }
 
